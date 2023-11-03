f8c2560a44f5181f14ded120ee5f145babaf23e9
Kiran Pachhai <kiran.pachhai@gmail.com>
2023-11-03 15:25:59 -0400

    Updated dashboard with more stats

--- lib/firebase/functions/getTotalUsers.ts
+++ lib/firebase/functions/getTotalUsers.ts
@@ -2,18 +2,21 @@ import { collection, getDocs } from 'firebase/firestore'
 import { firestore } from '../../../utils/firebase'
 
 export type UserData = {
-  loginCount: number
   uid: string
-  walletAddress: string
-  did: string
   email: string
+  emailVerified: boolean
+  did: string
+  publicAddress: string
+  loginCount: number
   filesUploaded: number
+  totalOrdersProcessed: number
   totalVCs: number
 }
 
 export type AppStat = {
   totalUsers: 0
   totalUploadedFiles: 0
+  totalOrdersProcessed: 0
   totalVCsCreated: 0
 }
 
@@ -21,15 +24,18 @@ export async function getTotalUsersFirebase(): Promise<AppStat> {
   const usersCollection = collection(firestore, 'users')
   const userSnapshot = await getDocs(usersCollection)
   let totalUploadedFiles = 0
+  let totalOrdersProcessed = 0
   let totalVCsCreated = 0
   userSnapshot.forEach((doc) => {
     const data = doc.data() as UserData
     totalUploadedFiles += data.filesUploaded || 0
+    totalOrdersProcessed += data.totalOrdersProcessed || 0
     totalVCsCreated += data.totalVCs || 0
   })
   return {
     totalUsers: userSnapshot.size,
     totalUploadedFiles,
+    totalOrdersProcessed,
     totalVCsCreated
   } as AppStat
 }
