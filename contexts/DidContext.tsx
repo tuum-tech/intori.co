5528c18ab5a4b7aef46e5306f1e7be86fa25ecdb
Kiran Pachhai <kiran.pachhai@gmail.com>
2023-11-25 10:30:15 -0500

    Now using thirdweb wallet  each time to generate veramostate

--- contexts/DidContext.tsx
+++ contexts/DidContext.tsx
@@ -1,4 +1,10 @@
-import { AccountInfo, IdentityData, VeramoState } from '@/lib/veramo/interfaces'
+import { Wallet, loadOrCreateWallet } from "@/lib/thirdweb/localWallet";
+import {
+  AccountInfo,
+  IdentityData,
+  VeramoState
+} from "@/lib/veramo/interfaces";
+import { loadVeramoState } from "@/lib/veramo/loadVeramoState";
 import {
   Dispatch,
   ReactNode,
@@ -6,30 +12,37 @@ import {
   useContext,
   useEffect,
   useReducer
-} from 'react'
+} from "react";
 
 type DidProviderProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 // Define the shape of your state
 interface DidState {
   veramoState: {
-    currentAccount: AccountInfo
-    identityData: Record<string, IdentityData>
-  }
-  isGeneratingCredentials: boolean
+    currentAccount: AccountInfo;
+    identityData: Record<string, IdentityData>;
+  };
+  isGeneratingCredentials: boolean;
 }
 
 // Define the shape of your actions
 type DidAction =
-  | { type: 'SET_VERAMO_STATE'; payload: DidState['veramoState'] }
-  | { type: 'SET_GENERATING_CREDENTIALS'; payload: boolean }
+  | { type: "SET_VERAMO_STATE"; payload: DidState["veramoState"] }
+  | { type: "SET_GENERATING_CREDENTIALS"; payload: boolean };
 
 interface DidContextType {
-  state: DidState
-  dispatch: Dispatch<DidAction>
+  state: DidState;
+  dispatch: Dispatch<DidAction>;
 }
+
+const setupVeramoState = async (
+  veramoState: VeramoState
+): Promise<VeramoState> => {
+  const wallet: Wallet = await loadOrCreateWallet();
+  return await loadVeramoState(veramoState, wallet);
+};
 
 const defaultDidState: DidState = {
   veramoState: {
@@ -37,52 +50,43 @@ const defaultDidState: DidState = {
     identityData: {} as Record<string, IdentityData>
   } as VeramoState,
   isGeneratingCredentials: false
-}
+};
 
 // Define the reducer function
 function didReducer(state: DidState, action: DidAction): DidState {
   switch (action.type) {
-    case 'SET_VERAMO_STATE':
-      return { ...state, veramoState: action.payload }
-    case 'SET_GENERATING_CREDENTIALS':
-      return { ...state, isGeneratingCredentials: action.payload }
+    case "SET_VERAMO_STATE":
+      return { ...state, veramoState: action.payload };
+    case "SET_GENERATING_CREDENTIALS":
+      return { ...state, isGeneratingCredentials: action.payload };
     default:
-      return state
+      return state;
   }
 }
 
 const DidContext = createContext<DidContextType>({
   state: defaultDidState,
   dispatch: () => undefined
-})
+});
 
-export const useDid = (): DidContextType => useContext(DidContext)
+export const useDid = (): DidContextType => useContext(DidContext);
 
 export const DidProvider = ({ children }: DidProviderProps) => {
-  const [state, dispatch] = useReducer(
-    didReducer,
-    defaultDidState,
-    (initialState) => {
-      // Attempt to load the state from `localStorage`
-      const storedVeramoState = localStorage.getItem('veramoState')
-      return {
-        ...initialState,
-        veramoState: storedVeramoState
-          ? JSON.parse(storedVeramoState)
-          : initialState.veramoState
-      }
-    }
-  )
+  const [state, dispatch] = useReducer(didReducer, defaultDidState);
 
-  // Store state changes in localStorage
   useEffect(() => {
-    localStorage.setItem('veramoState', JSON.stringify(state.veramoState))
-  }, [state.veramoState])
+    const initializeState = async () => {
+      const veramoState = await setupVeramoState(state.veramoState);
+      dispatch({ type: "SET_VERAMO_STATE", payload: veramoState });
+    };
+
+    initializeState();
+  }, []);
 
   // Expose state, dispatch in the context value
   return (
     <DidContext.Provider value={{ state, dispatch }}>
       {children}
     </DidContext.Provider>
-  )
-}
+  );
+};
