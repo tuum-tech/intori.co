31b48eb93145b07d47ca4059975c1ae4828d7d9e
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-04-18 10:54:15 -0400

    Updated the names for env variable

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -5,24 +5,24 @@
 // rpcUrl	  string	No	Optimism RPC server URL	            https://mainnet.optimism.io
 // version	string	No	Farcaster Auth version	            v1
 declare interface AuthKitConfig {
-    relay?: string;
-    domain?: string;
-    siweUri?: string;
-    rpcUrl?: string;
-    version?: string;
+  relay?: string
+  domain?: string
+  siweUri?: string
+  rpcUrl?: string
+  version?: string
 }
 
 export const domainFromNextUrl = () => {
-  const url = process.env.NEXTAUTH_URL || 'http://localhost:3000'
+  const url = process.env.NEXT_PUBLIC_AUTH_URL || 'http://localhost:3000'
   return new URL(url).host
 }
 
 export const authKitConfig: AuthKitConfig = {
   // For a production app, replace this with an Optimism Mainnet
   // RPC URL from a provider like Alchemy or Infura.
-  rpcUrl: "https://mainnet.optimism.io",
+  rpcUrl: 'https://mainnet.optimism.io',
   domain: domainFromNextUrl(),
 
   // extra slash at end is important.
-  siweUri: (process.env.NEXTAUTH_URL || 'http://localhost:3000') + '/'
+  siweUri: (process.env.NEXT_PUBLIC_AUTH_URL || 'http://localhost:3000') + '/'
 }
