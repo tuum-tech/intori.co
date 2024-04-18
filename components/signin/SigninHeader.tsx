8df88f5ddd124b41c682aff718f66be5828cf1c5
zacharytyhacz <zactyh@gmail.com>
2024-04-17 22:26:46 -0400

    Added 'sign in with farcaster' btn on home

--- components/signin/SigninHeader.tsx
+++ components/signin/SigninHeader.tsx
@@ -10,7 +10,7 @@ const SigninHeader: NextPage = () => {
       />
       <div className='self-stretch flex flex-col items-center justify-start gap-[12px]'>
         <div className='self-stretch relative font-semibold'>
-          Sign in to Intori
+          Sign in to Intori with Farcaster
         </div>
         <div className='w-full relative text-sm leading-[22px] font-light text-white-1 inline-block max-w-[300px]'>
           Get paid for what you already do and earn from online activity.
