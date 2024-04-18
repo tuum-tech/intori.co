f8313921bd2749e9439171ff68af664125681a08
zacharytyhacz <zactyh@gmail.com>
2024-04-17 22:27:01 -0400

    Added nextauth session provider around app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,10 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react";
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { Fragment, useEffect, useMemo } from 'react'
+import { Fragment } from 'react'
 import './global.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  // TODO: NextAuth hook with Farcaster auth integration will determine
-  const isLoggedIn = useMemo(() => false, [])
-  const loading = useMemo(() => false, [])
-  const router = useRouter()
-
-  useEffect(() => {
-    if (!loading) {
-      if (!isLoggedIn && router.pathname !== '/') {
-        router.push('/')
-      } else if (isLoggedIn && router.pathname === '/') {
-        router.push('/dashboard')
-      }
-    }
-  }, [isLoggedIn, loading, router])
-
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <Fragment>
       <Head>
@@ -29,8 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
       </Head>
-      { /* TODO: Add NextAuth provider here */ }
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </Fragment>
   )
 }
