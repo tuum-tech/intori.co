9f05a0aace09da23090a5350acc15894404c48ff
zacharytyhacz <zactyh@gmail.com>
2024-04-23 16:01:18 -0400

    Added initial frame setup routes

---
+++ pages/frames/initial.tsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import { FarcasterFrameHead } from '../../components/farcaster-frames/FarcasterFrameHead'
+import { PageWrapper } from '../../components/farcaster-frames/PageWrapper'
+ 
+type Props = {
+  currentStep: number
+  imageUrl: string
+  postUrl: string
+}
+ 
+export const getServerSideProps = (async (context) => {
+  const currentStep = parseInt(context.query.step as string, 10) || 0
+  const imageUrl = process.env.NEXTAUTH_URL + '/assets/img-step-1.png'
+  const postUrl = `${process.env.NEXTAUTH_URL}/frames/initial?id=${Date.now()}&step=${currentStep + 1}`
+
+  return {
+    props: {
+      currentStep: currentStep,
+      imageUrl,
+      postUrl
+    }
+  }
+}) satisfies GetServerSideProps<Props>
+ 
+export default function Page({
+  currentStep,
+  imageUrl,
+  postUrl
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!currentStep) {
+    return (
+      <PageWrapper title="Continuous profile building experience">
+        <FarcasterFrameHead title="Intori" imgUrl={imageUrl}>
+          <meta name="fc:frame:post_url" content={postUrl} />
+          <meta name="fc:frame:button:1" content="Go!" />
+
+          <meta name="fc:frame:button:2" content="Learn More" />
+          <meta name="fc:frame:button:2:action" content="link" />
+          <meta name="fc:frame:button:2:target" content="https://www.intori.co/" />
+        </FarcasterFrameHead>
+
+        <form method="POST" action="/api/frames/submit">
+          <p>Sorry, but right now this form is only accessible on Warpcast and Farcaster clients.</p>
+          <button type="submit">Submit</button>
+        </form>
+      </PageWrapper>
+    )
+  }
+
+  if (currentStep === 1) {
+    return (
+      <PageWrapper title="Professional Experience">
+        <FarcasterFrameHead title="Professional Experience" imgUrl={imageUrl}>
+          <meta name="fc:frame:post_url" content={postUrl} />
+          <meta name="fc:frame:button:1" content="Employed" />
+          <meta name="fc:frame:button:2" content="Self-employed" />
+          <meta name="fc:frame:button:3" content="Freelancer" />
+          <meta name="fc:frame:button:4" content="More" /> {/* TODO: add 'more' check */}
+        </FarcasterFrameHead>
+        <form method="POST" action={postUrl}>
+          <p>Sorry, but right now this form is only accessible on Warpcast and Farcaster clients.</p>
+        </form>
+      </PageWrapper>
+    )
+  }
+
+  if (currentStep === 2) {
+    return (
+      <PageWrapper title="Professional Experience">
+        <FarcasterFrameHead title="Professional Experience" imgUrl={imageUrl}>
+          <meta name="fc:frame:post_url" content={postUrl} />
+          <meta name="fc:frame:button:1" content="Student" />
+          <meta name="fc:frame:button:2" content="Retired" />
+          <meta name="fc:frame:button:3" content="Job Seeking" />
+          <meta name="fc:frame:button:4" content="Other" />
+        </FarcasterFrameHead>
+        <form method="POST" action={postUrl}>
+          <p>Sorry, but right now this form is only accessible on Warpcast and Farcaster clients.</p>
+        </form>
+      </PageWrapper>
+    )
+  }
+
+  if (currentStep === 3) {
+    return (
+      <PageWrapper title="Skills and Endorsements">
+        <FarcasterFrameHead title="Skills and Endorsements" imgUrl={imageUrl}>
+          <meta name="fc:frame:post_url" content={postUrl} />
+          <meta name="fc:frame:button:1" content="Programming" />
+          <meta name="fc:frame:button:2" content="Design" />
+          <meta name="fc:frame:button:3" content="Management" />
+          <meta name="fc:frame:button:4" content="More" />
+        </FarcasterFrameHead>
+        <form method="POST" action={postUrl}>
+          <p>Sorry, but right now this form is only accessible on Warpcast and Farcaster clients.</p>
+        </form>
+      </PageWrapper>
+    )
+  }
+
+  if (currentStep === 4) {
+    return (
+      <PageWrapper title="Skills and Endorsements">
+        <FarcasterFrameHead title="Skills and Endorsements" imgUrl={imageUrl}>
+          <meta name="fc:frame:post_url" content={postUrl} />
+          <meta name="fc:frame:button:1" content="Sales" />
+          <meta name="fc:frame:button:2" content="Marketing" />
+          <meta name="fc:frame:button:3" content="Education" />
+          <meta name="fc:frame:button:4" content="Other" />
+        </FarcasterFrameHead>
+        <form method="POST" action={postUrl}>
+          <p>Sorry, but right now this form is only accessible on Warpcast and Farcaster clients.</p>
+        </form>
+      </PageWrapper>
+    )
+  }
+
+  if (currentStep === 5) {
+    return (
+      <PageWrapper title="Congrats – Your profile is growing!">
+        <FarcasterFrameHead title="Congrats – Your profile is growing!" imgUrl={imageUrl}>
+          <meta name="fc:frame:post_url" content={postUrl} />
+          <meta name="fc:frame:button:2" content="View Profile" />
+          <meta name="fc:frame:button:2:action" content="link" />
+          <meta name="fc:frame:button:2:target" content="https://www.intori.co/" />
+
+          <meta name="fc:frame:button:1" content="Share" />
+        </FarcasterFrameHead>
+        <form method="POST" action={postUrl}>
+          <p>Sorry, but right now this form is only accessible on Warpcast and Farcaster clients.</p>
+        </form>
+      </PageWrapper>
+    )
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,10 @@
-import { useRouter } from 'next/router'
-import { useSession, signIn, signOut, getCsrfToken } from "next-auth/react"
-import { useCallback, useState, useEffect } from 'react'
-import {
-  SignInButton,
-  AuthKitProvider,
-  StatusAPIResponse,
-} from "@farcaster/auth-kit"
 import "@farcaster/auth-kit/styles.css"
 
-import { authKitConfig } from '../utils/farcaster'
-
-import SigninHeader from '../components/signin/SigninHeader'
+import { SignIn } from '../components/signin'
 
 const SigninDefaultScreen = () => {
-    const router = useRouter()
-
-    const session = useSession()
-    const [error, setError] = useState(false)
-
-    useEffect(() => {
-        console.log({ session })
-    }, [session])
-
-    // A nonce ensures that each authentication request is unique.
-    // It prevents attackers from capturing a valid authentication request and replaying
-    // it to gain unauthorized access, because replaying the request with the same nonce
-    // would immediately be flagged as invalid by the system.
-    const getNonce = useCallback(async () => {
-        const nonce = await getCsrfToken()
-        if (!nonce) throw new Error("Unable to generate nonce")
-        return nonce
-    }, [])
-
-    const handleSuccess = useCallback(
-        async (res: StatusAPIResponse) => {
-            console.log({ res })
-            await signIn("credentials", {
-                message: res.message,
-                signature: res.signature,
-                name: res.username,
-                pfp: res.pfpUrl,
-                redirect: false,
-            })
-
-            router.push('/dashboard')
-        },
-        [signIn, router]
-    )
-
   return (
-      <AuthKitProvider config={authKitConfig}>
-          <div className='relative bg-black-0 w-full h-screen flex flex-col items-start justify-start text-center text-11xl text-white font-kumbh-sans sm:pl-0 sm:pr-0 sm:box-border'>
-              <div className='self-stretch flex-1 overflow-hidden flex flex-row items-center justify-center h-screen lg:flex-row md:flex-row sm:items-center sm:justify-center'>
-                  <div className='self-stretch flex-1 flex flex-col items-center justify-center py-10 px-3 box-border max-w-[400px] h-screen lg:h-screen md:gap-[0px] md:items-center md:justify-center md:pl-0 md:pr-0 md:box-border md:max-w-[450px] md:h-screen sm:w-full sm:flex-col sm:gap-[5px] sm:items-center sm:justify-center sm:pl-0 sm:pt-10 sm:pr-0 sm:box-border sm:max-w-[450px] sm:h-screen Small_Tablet:pl-0 Small_Tablet:pr-0 Small_Tablet:box-border'>
-                      <SigninHeader />
-                      <div className='self-stretch flex flex-col items-center justify-center gap-[25px] text-left text-sm'>
-                          <SignInButton
-                              nonce={getNonce}
-                              onSuccess={handleSuccess}
-                              onError={() => setError(true)}
-                              onSignOut={() => signOut() }
-                          />
-                          {error && <div>Unable to sign in at this time.</div>}
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </AuthKitProvider>
+    <SignIn redirectAfterLogin='/dashboard' />
   )
 }
 
