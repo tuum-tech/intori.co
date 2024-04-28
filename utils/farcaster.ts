e1ac3c314caf7d540524949b726c5fd4c67d2ca9
zacharytyhacz <zactyh@gmail.com>
2024-04-28 11:26:14 -0400

    moved server side only pkg usage to server only

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -1,14 +1,3 @@
-import {
-  CastAddMessage,
-  fromFarcasterTime,
-  getSSLHubRpcClient,
-  getInsecureHubRpcClient,
-  HubAsyncResult,
-  HubRpcClient,
-  isCastAddMessage,
-  isUserDataAddMessage,
-  UserDataType,
-} from "@farcaster/hub-nodejs";
 
 //          Type	Required	Description	                Default
 // relay	  string	No	Farcaster Auth relay server URL	    https://relay.farcaster.xyz
@@ -48,32 +37,4 @@ export const authKitConfig: AuthKitConfig = {
 
   // extra slash at end is important.
   siweUri: getAppUri()
-}
-
-export const getUserProfilePictureFromFid = async (
-  fid: number
-): Promise<string> => {
-  if (!process.env.HUB_URL) {
-    console.error('No HUB_URL provided.')
-    return ''
-  }
-
-  const client = getSSLHubRpcClient(process.env.HUB_URL);
-
-  const result = await client.getUserData({
-    fid,
-    userDataType: UserDataType.PFP
-  })
-
-  client.close()
-
-  let profileImageUrl: string = ''
-  result.map((user) => {
-    if (!user?.data?.userDataBody) {
-      return null
-    }
-    profileImageUrl = user.data.userDataBody.value
-  })
-
-  return profileImageUrl
 }
