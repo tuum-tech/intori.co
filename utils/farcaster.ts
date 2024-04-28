9fa1f324da147b19e6db6445b339b7af36d74d42
zacharytyhacz <zactyh@gmail.com>
2024-04-28 00:22:36 -0400

    Added getting of image by fid

--- utils/farcaster.ts
+++ utils/farcaster.ts
@@ -1,3 +1,15 @@
+import {
+  CastAddMessage,
+  fromFarcasterTime,
+  getSSLHubRpcClient,
+  getInsecureHubRpcClient,
+  HubAsyncResult,
+  HubRpcClient,
+  isCastAddMessage,
+  isUserDataAddMessage,
+  UserDataType,
+} from "@farcaster/hub-nodejs";
+
 //          Type	Required	Description	                Default
 // relay	  string	No	Farcaster Auth relay server URL	    https://relay.farcaster.xyz
 // domain	  string	Yes	Domain of your application.	        None
@@ -36,4 +48,32 @@ export const authKitConfig: AuthKitConfig = {
 
   // extra slash at end is important.
   siweUri: getAppUri()
+}
+
+export const getUserProfilePictureFromFid = async (
+  fid: number
+): Promise<string> => {
+  if (!process.env.HUB_URL) {
+    console.error('No HUB_URL provided.')
+    return ''
+  }
+
+  const client = getSSLHubRpcClient(process.env.HUB_URL);
+
+  const result = await client.getUserData({
+    fid,
+    userDataType: UserDataType.PFP
+  })
+
+  client.close()
+
+  let profileImageUrl: string = ''
+  result.map((user) => {
+    if (!user?.data?.userDataBody) {
+      return null
+    }
+    profileImageUrl = user.data.userDataBody.value
+  })
+
+  return profileImageUrl
 }
