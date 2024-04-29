27e1a335719c6dee19951acbbf50198af7cd9c35
zacharytyhacz <zactyh@gmail.com>
2024-04-29 00:55:59 -0400

    Better app layout component

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import TopNavigationMenu from "@/components/top-navigation/TopNavigationMenu";
-import SideNavigationMenu from "../components/side-navigation/SideNavigationMenu";
+import Image from 'next/image'
 
 type Props = {
   title: string
@@ -9,23 +8,18 @@ type Props = {
 
 export const AppLayout: React.FC<Props> = ({ title, children }) => {
     return (
-      <div className="relative bg-black-0 w-full h-screen overflow-y-auto flex flex-row items-start justify-start text-left text-base text-white font-kumbh-sans">
-        <SideNavigationMenu />
-        <div className="self-stretch flex-1 overflow-y-auto flex flex-col items-center justify-start p-6 text-xs">
-          <div className="w-full flex flex-col items-start justify-start pt-0 px-0 pb-[50px] box-border gap-[24px] max-w-[1100px]">
-            <TopNavigationMenu />
-            <div className="self-stretch h-14 flex flex-col items-start justify-center text-13xl md:pl-3 md:box-border">
-              <div className="flex flex-row items-center justify-start Small_Tablet:flex">
-                <h1 className="m-0 relative text-inherit font-semibold font-inherit Small_Tablet:flex">
-                  { title }
-                </h1>
-              </div>
-            </div>
-            <div className="self-stretch flex flex-row items-start justify-start gap-[24px] text-lg md:flex-col">
-              { children }
-            </div>
-          </div>
+      <div className="bg-black-0 w-full h-screen overflow-y-auto flex flex-col font-kumbh-sans">
+        <div className='flex flex-col items-center justify-start py-4 px-3 gap-[8px] lg:pl-3 lg:pr-3 lg:box-border md:pl-3 md:pr-3 md:box-border sm:pl-3 sm:pr-3 sm:box-border Small_Tablet:pl-3 Small_Tablet:pr-3 Small_Tablet:box-border'>
+          <Image
+            className='relative w-[26px] h-[35px]'
+            alt='Intori'
+            src='/intorilogomark.svg'
+            width={26}
+            height={35}
+          />
+          <h1 className="text-white">{title}</h1>
         </div>
+          { children }
       </div>
     )
 }
