7d6becdee0eb3078b429a687d1a8c8b2570f6273
zacharytyhacz <zactyh@gmail.com>
2024-05-02 12:19:41 -0400

    Added post install copying of fonts to public folder for use in profile frame jimp

--- pages/api/profile/[fid].ts
+++ pages/api/profile/[fid].ts
@@ -53,7 +53,9 @@ const getProfileFramePictureImage = async (
   const currentStreakDays = await findLongestStreak(fid)
   const streakText = currentStreakDays === 1 ? '1 day' : `${currentStreakDays} days`
 
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
+  const font = await Jimp.loadFont(
+    path.join(process.cwd(), 'public/fonts/open-sans-32-white/open-sans-32-white.fnt')
+  )
   baseImageWithProfilePic.print(font, 1226/2, 457/2, questionsAnswered.toString())
   baseImageWithProfilePic.print(font, 1226/2, 601/2, pointsEarned.toString())
   baseImageWithProfilePic.print(font, 1226/2, 744/2, streakText)
