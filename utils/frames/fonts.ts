212f6720cb0b15fa671263cf2c0015983e5eb6bb
zacharytyhacz <zactyh@gmail.com>
2024-05-07 15:29:07 -0400

    Added debug logs for paths

--- utils/frames/fonts.ts
+++ utils/frames/fonts.ts
@@ -2,6 +2,12 @@ import * as path from 'path'
 import Jimp from 'jimp'
 
 export const loadKumbSans32 = async () => {
+  console.log({
+    cwd: process.cwd(),
+    '__dirname': __dirname,
+    '__filename': __filename
+  })
+
   const pathToFont = path.join(
     process.cwd(),
     'public/assets/frames/kumbh_sans_32.fnt'
