43e0cff0a42532cf5b7a6db149c91d549bb5018e
zacharytyhacz <zactyh@gmail.com>
2024-05-16 21:39:13 -0400

    Cleaning up debug logs

--- utils/frames/fonts.ts
+++ utils/frames/fonts.ts
@@ -2,12 +2,6 @@ import * as path from 'path'
 import Jimp from 'jimp'
 
 export const loadKumbSans32 = async () => {
-  console.log({
-    cwd: process.cwd(),
-    '__dirname': __dirname,
-    '__filename': __filename
-  })
-
 
   const pathToFont = path.join(
     process.cwd(),
@@ -22,11 +16,6 @@ export const loadKumbSans32 = async () => {
   const fntExists = path.resolve(pathToFont)
   const pngExists = path.resolve(pathToPng)
 
-  console.log({
-    fntExists,
-    pngExists
-  })
-
   return await Jimp.loadFont(pathToFont)
 }
 
@@ -35,6 +24,5 @@ export const loadKumbSans21 = async () => {
     process.cwd(),
     'public/assets/frames/kumbh_sans_21.fnt'
   )
-  console.log('resulting path for 21 :', pathToFont)
   return await Jimp.loadFont(pathToFont)
 }
