36c54464e4cabc5b5a5fec5a1ac5b8273df3a2f6
zacharytyhacz <zactyh@gmail.com>
2024-05-21 12:55:28 -0400

    Added auth layout for home/login page(s)

---
+++ layouts/Auth/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Img from 'next/image'
+import styles from './Auth.module.css'
+
+type Props = {
+  children: React.ReactNode
+  title: JSX.Element | React.ReactNode
+  subtitle: string
+}
+
+export const AuthLayout: React.FC<Props> = ({
+  title,
+  subtitle,
+  children
+}) => {
+    return (
+      <div className={styles.authLayout}>
+        <div className={styles.logo}>
+          <Img src="/intori-logo-full.svg" alt="Intori" width={68} height={30} />
+        </div>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        {children}
+      </div>
+    )
+}
+
