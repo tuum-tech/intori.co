448840ae2b4da0a7219c4cfaa42a3c26818d897e
zacharytyhacz <zactyh@gmail.com>
2024-06-03 11:04:42 -0400

    Linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,12 +56,12 @@ const SigninDefaultScreen = () => {
 
         <details>
           <summary>What problem is Intori solving?</summary>
-          <p>In today's screen-driven world, we spend most of our waking hours online but receive little monetary benefit, even though the data we produce has become highly valuable for businesses. With Intori, consumers can easily connect, convert data, and create verifiable credentials, to be purchased by businesses on a marketplace to drive informed data-driven decisions.</p>
+          <p>In today&apos;s screen-driven world, we spend most of our waking hours online but receive little monetary benefit, even though the data we produce has become highly valuable for businesses. With Intori, consumers can easily connect, convert data, and create verifiable credentials, to be purchased by businesses on a marketplace to drive informed data-driven decisions.</p>
         </details>
 
         <details>
           <summary>Why is Intori the solution and not other data marketplaces?</summary>
-          <p>One reason is that the technology wasn't available. Decentralized Identifiers (DIDs) and AI are relatively new technologies, and they haven't been widely adopted yet. Additionally, in the past, most people were happy to share their data with businesses in exchange for services. However, as people have become more aware of the implications of sharing data, there’s a growing demand for less intrusive solutions. Technology we’ve been developing for 5+ years is the backbone of intori and allows users to share data in a way that is more secure, private, and impossible to achieve previously. We believe the growing demand for data-driven decision making will incentivize businesses to stay competitive and encourage finding ways to collect and use data without violating user privacy.</p>
+          <p>One reason is that the technology wasn&apos;t available. Decentralized Identifiers (DIDs) and AI are relatively new technologies, and they haven&apos;t been widely adopted yet. Additionally, in the past, most people were happy to share their data with businesses in exchange for services. However, as people have become more aware of the implications of sharing data, there’s a growing demand for less intrusive solutions. Technology we’ve been developing for 5+ years is the backbone of intori and allows users to share data in a way that is more secure, private, and impossible to achieve previously. We believe the growing demand for data-driven decision making will incentivize businesses to stay competitive and encourage finding ways to collect and use data without violating user privacy.</p>
         </details>
 
         <details>
