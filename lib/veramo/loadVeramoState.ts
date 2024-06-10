48c3c104f7155fbe2d2b9d72603fdaa89d223f79
zacharytyhacz <zactyh@gmail.com>
2024-06-10 17:23:35 -0400

    Added original lib/veramo files

---
+++ lib/veramo/loadVeramoState.ts
@@ -0,0 +1,104 @@
+import type { MinimalImportableKey } from '@veramo/core'
+import type { Wallet } from '../thirdweb/localWallet'
+import type { AccountInfo, VeramoState } from './interfaces'
+import { getVeramoAgent } from './setup'
+import { initAccountState } from './state/account'
+import { convertChainIdFromHex } from './state/network'
+import { CHAIN_ID } from './types/constants'
+
+export async function loadVeramoState(
+  state: VeramoState,
+  wallet: Wallet
+): Promise<VeramoState> {
+  try {
+    const evmAddress = wallet.address
+
+    // Initialize if not there
+    if (evmAddress && !(evmAddress in state.identityData)) {
+      console.log(
+        `The address ${evmAddress} has NOT yet been configured. Configuring now...`
+      )
+      state = await initAccountState(evmAddress)
+    }
+    return await veramoImportAccount(state, wallet)
+  } catch (e) {
+    console.error(`Error while trying to get the account: ${e}`)
+    throw new Error(`Error while trying to get the account: ${e}`)
+  }
+}
+
+export async function veramoImportAccount(
+  state: VeramoState,
+  wallet: Wallet
+): Promise<VeramoState> {
+  const chainId = CHAIN_ID // Only dealing with ETH Mainnet for now
+
+  const privateKey = wallet.privateKey
+  const publicKey = wallet.publicKey
+  const address = wallet.address.toLowerCase()
+
+  const method = state.currentAccount.didMethod
+
+  let did = ''
+  if (method === 'did:pkh') {
+    did = `did:pkh:eip155:${convertChainIdFromHex(chainId)}:${address}`
+  } else {
+    console.error(
+      `Error while trying to generate DID. Invalid did method: "${method}`
+    )
+    throw new Error(
+      `Error while trying to generate DID. Invalid did method: "${method}`
+    )
+  }
+
+  if (!did) {
+    console.log('Failed to generate DID')
+    throw new Error('Failed to generate DID')
+  }
+
+  state.currentAccount.evmAddress = address
+
+  // Get Veramo agent
+  const agent = await getVeramoAgent(state)
+  const controllerKeyId = `intori-${address}`
+  console.log(
+    `Importing using did=${did}, provider=${method}, controllerKeyId=${controllerKeyId}...`
+  )
+
+  // Get identifier if it exists
+  try {
+    await agent.didManagerGet({
+      did
+    })
+  } catch (error) {
+    try {
+      await agent.didManagerImport({
+        did,
+        provider: method,
+        controllerKeyId,
+        keys: [
+          {
+            kid: controllerKeyId,
+            type: 'Secp256k1',
+            kms: 'intori',
+            privateKeyHex: privateKey.replace(/^0x/, ''),
+            publicKeyHex: publicKey.replace(/^0x/, '')
+          } as MinimalImportableKey
+        ]
+      })
+    } catch (error) {
+      console.log('Error here: ', error)
+      throw new Error('Failed to import Did')
+    }
+  }
+  console.log('Imported successfully')
+
+  const account: AccountInfo = {
+    evmAddress: address,
+    did: did,
+    didMethod: method
+  } as AccountInfo
+
+  state.currentAccount = account
+  return state
+}
