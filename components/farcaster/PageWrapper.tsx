70ab086565eecda227ce52ec9ccddbf79392f04c
zacharytyhacz <zactyh@gmail.com>
2024-06-10 11:39:28 -0400

    Removed unused components and linting errors; added TODO tags

--- components/farcaster/PageWrapper.tsx
+++ components/farcaster/PageWrapper.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {
@@ -11,10 +12,11 @@ export const PageWrapper: React.FC<Props> = ({
     return (
       <main className='relative bg-black-0 w-full h-screen overflow-y-auto text-base text-white font-kumbh-sans pt-4'>
         <div className="text-center">
-          <img
-            className='relative w-[26px] h-[35px]'
+          <Image
             alt=''
             src='/intorilogomark.svg'
+            width={26}
+            height={35}
           />
           <h1>Intori</h1>
         </div>
--- components/signin/SigninHeader.tsx
+++ components/signin/SigninHeader.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/image'
 import { NextPage } from 'next'
 
 const SigninHeader: NextPage = () => {
   return (
     <div className='self-stretch flex flex-col items-center justify-start py-0 px-3 gap-[24px] lg:pl-3 lg:pr-3 lg:box-border md:pl-3 md:pr-3 md:box-border sm:pl-3 sm:pr-3 sm:box-border Small_Tablet:pl-3 Small_Tablet:pr-3 Small_Tablet:box-border'>
-      <img
-        className='relative w-[26px] h-[35px]'
-        alt=''
+      <Image
+        alt='Intori'
         src='/intorilogomark.svg'
+        width={26}
+        height={35}
       />
       <div className='self-stretch flex flex-col items-center justify-start gap-[12px]'>
         <div className='self-stretch relative font-semibold'>
