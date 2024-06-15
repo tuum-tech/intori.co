a294cb68416b0de1bfb6159446886648aab38deb
zacharytyhacz <zactyh@gmail.com>
2024-06-15 01:02:34 -0400

    Formatting

--- lib/ethers/registerCredential.ts
+++ lib/ethers/registerCredential.ts
@@ -5,7 +5,9 @@ const provider = new ethers.providers.JsonRpcProvider(
   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? 'http://localhost:8545'
 )
 const signer = provider.getSigner()
-const contractAddress = (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || 'enter valid address to .env.local')
+const contractAddress = (
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? 'Enter valid address to .env.local'
+)
 
 const contract = new ethers.Contract(contractAddress, abi, signer)
 
