10b1480b6633126216cb5b44f02a1d72b4839f99
zacharytyhacz <zactyh@gmail.com>
2024-06-15 01:01:53 -0400

    Added initial smart contract registering

---
+++ lib/ethers/registerCredential.ts
@@ -0,0 +1,32 @@
+import { ethers } from 'ethers'
+import abi from '../../contracts/CredentialRegistryABI.json'
+
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? 'http://localhost:8545'
+)
+const signer = provider.getSigner()
+const contractAddress = (process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || 'enter valid address to .env.local')
+
+const contract = new ethers.Contract(contractAddress, abi, signer)
+
+export async function registerCredential(
+  body: {
+    credentialId: string,
+    recipientDid: string,
+    credentialHash: string,
+    credentialType: string
+  }
+) {
+  const { credentialId, recipientDid, credentialHash, credentialType } = body
+
+  const tx = await contract.registerCredential(
+    credentialId,
+    recipientDid,
+    credentialHash,
+    credentialType
+  )
+
+  await tx.wait()
+
+  console.log('Transaction hash:', tx.hash)
+}
