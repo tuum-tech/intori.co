9cfd93aa4ab4dc542275adadff669418048688af
zacharytyhacz <zactyh@gmail.com>
2024-06-18 00:09:56 -0400

    Finally got smart contract interaction to work

--- lib/ethers/registerCredential.ts
+++ lib/ethers/registerCredential.ts
@@ -1,8 +1,29 @@
-import { ethers } from 'ethers'
+import { ethers, BigNumber } from 'ethers'
+import { VerifiableCredential, IIdentifier } from '@veramo/core'
 import abi from '../../contracts/CredentialRegistryABI.json'
 
+type TransactionType = {
+  hash: string;
+  type: number;
+  blockHash: string;
+  blockNumber: number;
+  transactionIndex: number;
+  confirmations: number;
+  from: string;
+  gasPrice: BigNumber;
+  gasLimit: BigNumber;
+  to: string;
+  value: BigNumber;
+  nonce: number;
+  data: string;
+  r: string;
+  s: string;
+  v: number;
+  chainId: number;
+}
+
 const provider = new ethers.providers.JsonRpcProvider(
-  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ?? 'http://localhost:8545'
+  process.env.NEXT_PUBLIC_RPC_URL ?? 'http://localhost:8545'
 )
 const signer = provider.getSigner()
 const contractAddress = (
@@ -12,23 +33,25 @@ const contractAddress = (
 const contract = new ethers.Contract(contractAddress, abi, signer)
 
 export async function registerCredential(
-  body: {
-    credentialId: string,
-    recipientDid: string,
-    credentialHash: string,
-    credentialType: string
-  }
-) {
-  const { credentialId, recipientDid, credentialHash, credentialType } = body
-
-  const tx = await contract.registerCredential(
-    credentialId,
-    recipientDid,
-    credentialHash,
-    credentialType
+  body: VerifiableCredential,
+  decentralizedIdentifier: IIdentifier
+): Promise<TransactionType> {
+  const credentialHash = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes(JSON.stringify(body.credentialSubject))
   )
 
-  await tx.wait()
+  const credentialId = ethers.utils.keccak256(
+    ethers.utils.toUtf8Bytes(body.credentialSubject.timestamp)
+  )
 
-  console.log('Transaction hash:', tx.hash)
+  const transaction = await contract.registerCredential(
+    credentialId,
+    decentralizedIdentifier.did,
+    credentialHash,
+    'IntoriAnswerCredential'
+  )
+
+  await transaction.wait()
+
+  return transaction
 }
