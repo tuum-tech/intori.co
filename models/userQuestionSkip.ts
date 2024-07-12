aa9be0a0bf015faa3ca81eb06f936566f796a4ab
zacharytyhacz <zactyh@gmail.com>
2024-07-12 13:04:57 -0400

    Keeping sort

--- models/userQuestionSkip.ts
+++ models/userQuestionSkip.ts
@@ -47,6 +47,7 @@ export const getLastSkippedQuestion = async (fid: number): Promise<UserQuestionS
   const query = await collection
     .where('fid', '==', fid)
     .orderBy('date', 'desc')
+    .limit(1)
     .get()
 
   if (query.docs.length === 0) {
