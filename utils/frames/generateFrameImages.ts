e9790292c58dea3456d49b161a5916fe7c59e844
zacharytyhacz <zactyh@gmail.com>
2024-07-18 11:28:04 -0400

    Added new question frame templates and logic to generate

--- utils/frames/generateFrameImages.ts
+++ utils/frames/generateFrameImages.ts
@@ -1,20 +1,25 @@
 import Jimp from 'jimp'
 import { Font } from '@jimp/plugin-print'
 import * as path from 'path'
-import { intoriQuestions } from './intoriFrameForms'
+import { intoriQuestions, IntoriQuestionType } from './intoriFrameForms'
 import {
-  loadKumbSans30
+  loadKumbSans46,
+  loadKumbSans26
 } from './fonts'
 
-let kumbhSans30: Font
+let kumbhSans46: Font
+let kumbhSans26: Font
 
 const loadFonts = async () => {
-  kumbhSans30 = await loadKumbSans30()
+  kumbhSans46 = await loadKumbSans46()
+  kumbhSans26 = await loadKumbSans26()
 }
 
 const generateQuestionnaireStepImage = async (
-  question: string
+  intoriQuestion: IntoriQuestionType
 ): Promise<Jimp> => {
+  const { question, answers } = intoriQuestion
+
   await loadFonts()
 
   const image = await Jimp.read(
@@ -25,17 +30,48 @@ const generateQuestionnaireStepImage = async (
   )
 
   image.print(
-    kumbhSans30,
-    103,
-    341,
+    kumbhSans46,
+    26,
+    171,
     {
       text: question,
       alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
       alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
     },
-    562,
-    74
+    715,
+    245
   )
+
+  const bullet = await Jimp.read(
+    path.join(process.cwd(), 'public/assets/templates/answer-bullet.png')
+  )
+
+  const leftColumnX = 129
+  const rightColumnX = 416
+  const rowOffset = 60
+
+  for (let i = 0; i < answers.length; i++) {
+    const answer = answers[i]
+    const bulletXPosition = i % 2 === 0 ? leftColumnX : rightColumnX
+    const bulletYPosition = 449 + Math.floor(i / 2) * rowOffset
+
+    // add bullet point image
+    image.composite(bullet, bulletXPosition, bulletYPosition)
+
+    // answer text
+    image.print(
+      kumbhSans26,
+      bulletXPosition + 27,
+      bulletYPosition - 20,
+      {
+        text: answer,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+      },
+      260,
+      57
+    )
+  }
 
   return image
 }
@@ -49,14 +85,14 @@ export const generateQuestionnaireStepImages = async () => {
   for (let i = 0; i < intoriQuestions.length; i++) {
     const questionIndex = i
 
-    const { question } = intoriQuestions[questionIndex]
+    const intoriQuestion = intoriQuestions[questionIndex]
 
     const questionImagePath = path.join(
       questionImagePathPrefix,
       questionIndex.toString() + '.png'
     )
 
-    const image = await generateQuestionnaireStepImage(question)
+    const image = await generateQuestionnaireStepImage(intoriQuestion)
 
     image.write(questionImagePath)
   }
