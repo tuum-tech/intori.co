1d67e31a48a45532ed33c2b02282b2022d3ce258
zacharytyhacz <zactyh@gmail.com>
2024-07-22 12:25:19 -0400

    Improving button and input styles

--- components/common/Button/index.tsx
+++ components/common/Button/index.tsx
@@ -5,18 +5,25 @@ type Props = {
   children: React.ReactNode
   onClick?: React.MouseEventHandler
   disabled?: boolean
+  type?: 'button' | 'submit' | 'reset'
+}
+
+const combineStyles = (styles: string[]) => {
+  return styles.join(' ')
 }
 
 export const PrimaryButton: React.FC<Props> = ({
   onClick,
   children,
   disabled,
+  type
 }) => {
   return (
     <button
-        className={styles.primaryButton}
+        className={combineStyles([styles.primaryButton, styles.button])}
         disabled={disabled}
         onClick={onClick}
+        type={type ?? 'button'}
     >
       { children }
     </button>
@@ -27,12 +34,32 @@ export const SecondaryButton: React.FC<Props> = ({
   onClick,
   children,
   disabled,
+  type
 }) => {
   return (
     <button
-        className={styles.secondaryButton}
+        className={combineStyles([styles.secondaryButton, styles.button])}
         disabled={disabled}
         onClick={onClick}
+        type={type ?? 'button'}
+    >
+      { children }
+    </button>
+  )
+}
+
+export const DangerButton: React.FC<Props> = ({
+  onClick,
+  children,
+  disabled,
+  type
+}) => {
+  return (
+    <button
+        className={combineStyles([styles.dangerButton, styles.button])}
+        disabled={disabled}
+        onClick={onClick}
+        type={type ?? 'button'}
     >
       { children }
     </button>
