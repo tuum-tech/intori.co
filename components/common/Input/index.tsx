59c174e2cec267188d202a7dadc5b524dc3d7a26
zacharytyhacz <zactyh@gmail.com>
2024-07-22 12:25:19 -0400

    Improving button and input styles

--- components/common/Input/index.tsx
+++ components/common/Input/index.tsx
@@ -1,14 +1,17 @@
-// Input.tsx
 import { NextPage } from 'next'
 import React from 'react'
+import styles from './styles.module.css'
 
 type Props = {
-  label: string
+  label?: string
   placeholder?: string
   value: string
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   onClick?: React.MouseEventHandler<HTMLInputElement>
   readOnly?: boolean
+  required?: boolean
+  name?: string
+  onBlur?: React.FocusEventHandler<HTMLInputElement>
 }
 
 const Input: NextPage<Props> = ({
@@ -17,20 +20,28 @@ const Input: NextPage<Props> = ({
   value,
   onChange,
   onClick,
-  readOnly
+  readOnly,
+  required,
+  name,
+  onBlur
 }) => {
   return (
-    <div className='self-stretch flex flex-col items-start justify-start gap-[10px]'>
-      <div className='self-stretch flex flex-row items-start justify-start py-2.5 pr-2.5 pl-0'>
-        <div className='flex-1 relative font-medium'>{label}</div>
-      </div>
+    <div className={styles.inputContainer}>
+      { !!label && (
+        <label htmlFor={name}>
+          {label }
+        </label>
+      )}
+
       <input
-        className='self-stretch rounded-mini box-border h-14 flex flex-row items-center justify-start pt-[15px] pb-4 pr-[25px] pl-5 bg-black-0 text-grey-1 border-[1px] border-solid border-black-4'
         value={value}
         onChange={onChange}
         placeholder={placeholder}
         onClick={onClick}
         readOnly={readOnly}
+        required={required}
+        onBlur={onBlur}
+        name={name}
       />
     </div>
   )
