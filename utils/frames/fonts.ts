f1acc9ed10d380ee41648131d2de1fe031993426
zacharytyhacz <zactyh@gmail.com>
2024-07-26 12:41:07 -0400

    Updated new way to load fonts and positioning of new results frame

--- utils/frames/fonts.ts
+++ utils/frames/fonts.ts
@@ -1,56 +1,25 @@
 import * as path from 'path'
 import Jimp from 'jimp'
 
-export const loadFont = async (fontName: string) => {
+export const loadFont = async (params: {
+  family: 'kumbh_sans'
+  weight: 'regular' | 'medium' | 'light' | 'semibold'
+  size: number
+  color: 'white' | 'black' | 'grey'
+}) => {
+  const fileName = `${params.family}_${params.size}_${params.weight}_${params.color}.fnt`
+
   const pathToFont = path.join(
     process.cwd(),
     'public/assets/fonts/',
-    fontName
+    fileName
   )
 
+  try {
+    await Jimp.read(pathToFont)
+  } catch (error) {
+    throw new Error(`Font file ${fileName} does not exist`)
+  }
+
   return await Jimp.loadFont(pathToFont)
-}
-
-export const loadKumbSans50 = async () => {
-  return await loadFont('kumbh_sans_50.fnt')
-}
-
-export const loadKumbSans46 = async () => {
-  return await loadFont('kumbh_sans_46.fnt')
-}
-
-export const loadKumbSans32 = async () => {
-  return await loadFont('kumbh_sans_32.fnt')
-}
-
-export const loadKumbSans30 = async () => {
-  return await loadFont('kumbh_sans_30.fnt')
-}
-
-export const loadKumbSans26 = async () => {
-  return await loadFont('kumbh_sans_26.fnt')
-}
-
-export const loadKumbSans26medium = async () => {
-  return await loadFont('kumbh_sans_26_medium.fnt')
-}
-
-export const loadKumbSans24 = async () => {
-  return await loadFont('kumbh_sans_24.fnt')
-}
-
-export const loadKumbSans21 = async () => {
-  return await loadFont('kumbh_sans_21.fnt')
-}
-
-export const loadKumbSans20 = async () => {
-  return await loadFont('kumbh_sans_20.fnt')
-}
-
-export const loadKumbSans18 = async () => {
-  return await loadFont('kumbh_sans_18.fnt')
-}
-
-export const loadKumbSans14 = async () => {
-  return await loadFont('kumbh_sans_14_regular.fnt')
 }
