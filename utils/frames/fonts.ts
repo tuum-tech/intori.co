81eb54afcc31ae9503772ab0425b869f07d50469
zacharytyhacz <zactyh@gmail.com>
2024-07-29 15:03:11 -0400

    Added file parsing and debug logs for font files

--- utils/frames/fonts.ts
+++ utils/frames/fonts.ts
@@ -1,5 +1,23 @@
 import * as path from 'path'
+import { promises as fs } from 'fs'
 import Jimp from 'jimp'
+
+const checkFileExists = async (filePath: string): Promise<boolean> => {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const listFolderContents = async (folderPath: string): Promise<void> => {
+  try {
+    const files = await fs.readdir(folderPath);
+  } catch (error) {
+    console.error(`Error reading directory ${folderPath}:`, error);
+  }
+};
 
 export const loadFont = async (params: {
   family: 'kumbh_sans'
@@ -14,6 +32,15 @@ export const loadFont = async (params: {
     'public/assets/fonts/',
     fileName
   )
+
+  const exists = await checkFileExists(pathToFont)
+  console.log('cwd:', process.cwd())
+  console.log(fileName, { exists })
+
+  await listFolderContents(process.cwd())
+  await listFolderContents(path.join(process.cwd(), 'public/assets/fonts/'))
+
+  // check if file exists with path
 
   return await Jimp.loadFont(pathToFont)
 }
