420e6f4b9b996611d26077637432d72103260eac
zacharytyhacz <zactyh@gmail.com>
2024-08-06 21:44:08 -0400

    Added most answered questions chart

--- components/Stats/StatsCard.tsx
+++ components/Stats/StatsCard.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Skeleton } from '../common/Skeleton'
 import styles from './styles.module.css'
 
 type Props = {
   title: string
   value: string | number
+}
+
+export const LoadingStatsCard: React.FC<{ title: string }> = ({
+  title
+}) => {
+    return (
+      <div className={styles.statsCard}>
+        <h6>{title}</h6>
+        <h3>
+          <Skeleton width={Math.floor(Math.random() * (200 - 120 + 1)) + 120} />
+        </h3>
+      </div>
+    )
 }
 
 export const StatsCard: React.FC<Props> = ({
