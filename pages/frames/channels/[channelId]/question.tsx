d9c4964437ff5c1aafb39eb52ccd35153dc26c09
zacharytyhacz <zactyh@gmail.com>
2024-08-13 13:17:13 -0400

    Added folder for channel id for intro frame and question frame

---
+++ pages/frames/channels/[channelId]/question.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import { FrameGenerator } from '../../../../components/farcaster/FrameGenerator'
+import { AppLayout } from "../../../../layouts/App"
+import { Section } from '../../../../components/common/Section'
+import {
+    IntoriFrameType,
+    createIntroductionStep
+} from '../../../../utils/frames/intoriFrameForms'
+import Input from '../../../../components/common/Input'
+import { PrimaryButton } from '../../../../components/common/Button'
+import styles from '../FramePage.module.css'
+
+// TODO: get channelId param and expect questionId query.
+// check if question by question id exists
+// create image url that will include question text and channel branding
+ 
+type Props = {
+  imageUrl: string
+  frameUrl: string
+  frame: IntoriFrameType
+}
+ 
+export const getServerSideProps = (async (context) => {
+  const channelId = context.params?.channelId?.toString()
+
+  if (!channelId) {
+    return {
+      notFound: true
+    }
+  }
+
+  const frameUrl = `${process.env.NEXTAUTH_URL}/frames/channels/${channelId}`
+  const imageUrl = `${process.env.NEXTAUTH_URL}/assets/frames/channels/${channelId}/intro.png`
+
+  return {
+    props: {
+      imageUrl,
+      frameUrl,
+      frame: createIntroductionStep({ channelId })
+    }
+  }
+}) satisfies GetServerSideProps<Props>
+ 
+export default function Page({
+  imageUrl,
+  frameUrl,
+  frame
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [copyButtonText, setCopyButtonText] = useState('Copy Frame Link')
+
+  const copyUrlToClipboard = () => {
+    navigator.clipboard.writeText(frameUrl)
+    toast.success('Frame link copied to clipboard 😎')
+    setCopyButtonText('Copied!')
+    setTimeout(() => {
+      setCopyButtonText('Copy Frame Link')
+    }, 2000)
+  }
+
+  return (
+    <>
+      <FrameGenerator
+        frame={frame}
+        imageUrl={imageUrl}
+        frameUrl={frameUrl}
+        frameImageAspectRatio="1:1"
+      />
+      <AppLayout>
+        <Section>
+          <div className={styles.shareFrameContainer}>
+            <div className="text-center">
+              <h1>Your data, connected.</h1>
+
+              <div className={styles.inputContainer}>
+                <Input
+                  label="Share this frame with others and gain points!"
+                  value={frameUrl}
+                  onChange={console.log}
+                  placeholder="Frame URL"
+                  onClick={copyUrlToClipboard}
+                  readOnly
+                />
+                <PrimaryButton onClick={copyUrlToClipboard}>
+                  {copyButtonText}
+                </PrimaryButton>
+              </div>
+            </div>
+          </div>
+        </Section>
+      </AppLayout>
+    </>
+  )
+}
