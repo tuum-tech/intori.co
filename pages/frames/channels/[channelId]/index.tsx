f6d75e129158b62b05b43189c1265bab9d63c228
zacharytyhacz <zactyh@gmail.com>
2024-08-14 16:20:16 -0400

    Corrected url for getting channel intro image

--- pages/frames/channels/[channelId]/index.tsx
+++ pages/frames/channels/[channelId]/index.tsx
@@ -38,7 +38,7 @@ export const getServerSideProps = (async (context) => {
   }
 
   const frameUrl = `${process.env.NEXTAUTH_URL}/frames/channels/${channelId}`
-  const imageUrl = `${process.env.NEXTAUTH_URL}/api/frames/channels/${channelId}/intro`
+  const imageUrl = `${process.env.NEXTAUTH_URL}/api/frames/channels/${channelId}/images/intro`
 
   return {
     props: {
