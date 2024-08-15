69a66eaeec0c78ca57ac549c876479c0127b5eef
zacharytyhacz <zactyh@gmail.com>
2024-08-15 16:36:11 -0400

    Added checking if the frame session is for an intro frame, sending of intro question frames

---
+++ pages/api/frames/channels/[channelId]/images/question.ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {
+  createFrameErrorUrl
+} from '@/utils/frames/generatePageUrls'
+import { getChannelFrame } from '@/models/channelFrames'
+import { getQuestionById } from '@/models/questions'
+import { getChannelDetails } from '@/utils/neynarApi'
+import { createChannelQuestionFrameImage } from '@/utils/frames/createChannelQuestionFrameImage'
+
+// add channel logo to top
+// add question text
+// add answers
+const createChannelQuestionImage = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const questionId = (req.query.qid ?? '').toString()
+  const channelId = (req.query.channelId ?? '').toString()
+
+  if (!questionId || !channelId) {
+    return res.redirect(createFrameErrorUrl())
+  }
+
+  const channelFrame = await getChannelFrame(channelId)
+
+  if (!channelFrame) {
+    return res.redirect(createFrameErrorUrl())
+  }
+
+  const channelDetails = await getChannelDetails(channelId)
+
+  if (!channelDetails) {
+    return res.redirect(createFrameErrorUrl())
+  }
+
+  const question = await getQuestionById(questionId)
+
+  if (!question) {
+    return res.redirect(createFrameErrorUrl())
+  }
+
+  const { imageUrl } = channelDetails
+
+  const buffer = await createChannelQuestionFrameImage({
+    question,
+    channelImageUrl: imageUrl as string
+  })
+
+  // cache for 7 days
+  res.setHeader('Cache-Control', 'public, max-age=604800, immutable')
+  res.setHeader('Content-Type', 'image/png')
+
+  return res.status(200).send(buffer)
+}
+
+export default createChannelQuestionImage
