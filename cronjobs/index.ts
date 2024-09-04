105329145a40be9d096ea4c4e25408cde03af9a2
zacharytyhacz <zactyh@gmail.com>
2024-09-04 11:06:33 -0400

    wip initial cronjob setup

---
+++ cronjobs/index.ts
@@ -0,0 +1,10 @@
+// cron job
+//
+//
+//
+// every day,
+//    for each channel frame, get first response of users,
+//        for each first response of a user,
+//              - check if it's divisibly 3 weeks since the first response
+//              - for this user, get top suggested user
+//              - direct cast this user as Intori user and direct cast intro message
