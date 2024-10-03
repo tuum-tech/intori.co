b8aba1efc2d6bbdd3cdbbf5019da3dcbbe33092f
zacharytyhacz <zactyh@gmail.com>
2024-10-03 15:24:06 -0400

    Added potential channel member

---
+++ models/potentialChannelMember.ts
@@ -0,0 +1,45 @@
+import { createDb } from '../pages/api/utils/firestore'
+
+export type PotentialChannelMemberType = {
+  fid: number
+  channelId: string
+}
+
+let collection: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>
+
+const getCollection = () => {
+  if (collection) {
+    return collection
+  }
+
+  const db = createDb()
+  return db.collection('potentialChannelMembers')
+}
+
+export const createPotentialChannelMember = async (body: {
+  fid: number
+  channelId: string
+}): Promise<PotentialChannelMemberType> => {
+  const collection = getCollection()
+  await collection.add(body)
+
+  return {
+    fid: body.fid,
+    channelId: body.channelId
+  }
+}
+
+export const deletePotentialChannelMember = async (params: {
+  fid: number
+  channelId: string
+}): Promise<void> => {
+  const collection = getCollection()
+  const snapshot = await collection.where('fid', '==', params.fid).where('channelId', '==', params.channelId).get()
+
+  if (snapshot.empty) {
+    throw new Error('Potential channel member not found')
+  }
+
+  const doc = snapshot.docs[0]
+  await collection.doc(doc.id).delete()
+}
