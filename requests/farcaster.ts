b06a11e3b187aab61b2b29c0a3fe2442a894fd51
zacharytyhacz <zactyh@gmail.com>
2024-10-08 00:01:58 -0400

    Added farcaster api route to get channel details

--- requests/farcaster.ts
+++ requests/farcaster.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
-import { FarcasterUserType, FarcasterCastType } from '../utils/neynarApi'
+import { FarcasterUserType, FarcasterCastType, FarcasterChannelType } from '../utils/neynarApi'
 
 export const getFarcasterUserDetails = (
   fid: number
@@ -11,4 +11,10 @@ export const getFarcasterCastDetails = (
   hash: string
 ): Promise<AxiosResponse<FarcasterCastType>> => {
   return axios.get(`/api/farcaster/cast/${hash}`)
+}
+
+export const getFarcasterChannelDetails = (
+  channelId: string
+): Promise<AxiosResponse<FarcasterChannelType>> => {
+  return axios.get(`/api/farcaster/channel/${channelId}`)
 }
