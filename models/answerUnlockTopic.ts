48fd7f9cb70caa31e8194062175fcee479c9d24c
zacharytyhacz <zactyh@gmail.com>
2025-05-06 14:57:26 -0400

    Added initial model for the answer unlock topics

---
+++ models/answerUnlockTopic.ts
@@ -0,0 +1,58 @@
+import { createDb } from '../pages/api/utils/firestore'
+
+export type AnswerUnlockTopicType = {
+  question: string
+  answer: string
+  unlockTopic: string[]
+}
+
+let frameSessionsCollection: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>
+
+const getCollection = () => {
+  if (frameSessionsCollection) {
+    return frameSessionsCollection
+  }
+
+  const db = createDb()
+  return db.collection('answerUnlockTopic')
+}
+
+export const createAnswerUnlockTopic = async (body: AnswerUnlockTopicType) => {
+  const collection = getCollection()
+
+  // check if already exists
+  const query = collection
+    .where('question', '==', body.question)
+    .where('answer', '==', body.answer)
+
+  const snapshot = await query.get()
+  if (!snapshot.empty) {
+    throw new Error('Answer unlock topic already exists')
+  }
+
+  const doc = await collection.add(body)
+
+  const ref = await doc.get()
+
+  return ref.data()
+}
+
+export const getAnswerUnlockTopic = async (
+  body: AnswerUnlockTopicType
+):Promise<AnswerUnlockTopicType | null> => {
+  const collection = getCollection()
+
+  const query = collection
+    .where('question', '==', body.question)
+    .where('answer', '==', body.answer)
+
+  const snapshot = await query.get()
+
+  if (snapshot.empty) {
+    return null
+  }
+
+  const data = snapshot.docs[0].data() as AnswerUnlockTopicType
+
+  return data
+}
