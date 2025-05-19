5c132f112ff7a15f6f91dcf1e6b961a17426f86e
zacharytyhacz <zactyh@gmail.com>
2025-05-19 10:31:36 -0400

    Moving firebase over to prisma

--- models/questionCategories.ts
+++ models/questionCategories.ts
@@ -1,100 +1,66 @@
-import { createDb } from '../pages/api/utils/firestore'
+import { type QuestionCategory } from "@prisma/client"
+import { prisma } from "@/prisma"
 
-export type QuestionCategoryType = {
+export const getQuestionCategories = async (
+  questionId: string
+): Promise<QuestionCategory[]> => {
+  return prisma.questionCategory.findMany({
+    where: { questionId }
+  })
+}
+
+export const addQuestionCategory = async (body: {
   questionId: string
   categoryId: string
-}
+}): Promise<QuestionCategory> => {
+  const alreadyExists = await prisma.questionCategory.findFirst({
+    where: {
+      questionId: body.questionId,
+      categoryId: body.categoryId
+    },
+    select: { id: true }
+  })
 
-let frameSessionsCollection: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>
-
-const getCollection = () => {
-  if (frameSessionsCollection) {
-    return frameSessionsCollection
-  }
-
-  const db = createDb()
-  return db.collection('questionCategories')
-}
-
-export const getQuestionCategories = async (questionId: string) => {
-  const collection = getCollection()
-
-  const query = collection.where('questionId', '==', questionId)
-  const snapshot = await query.get()
-
-  const questionCategories = snapshot.docs.map((doc) => doc.data() as QuestionCategoryType)
-
-  return questionCategories
-}
-
-export const addQuestionCategory = async (body: QuestionCategoryType) => {
-  const collection = getCollection()
-
-  // check if already exists
-  const query = collection
-    .where('questionId', '==', body.questionId)
-    .where('categoryId', '==', body.categoryId)
-  const snapshot = await query.get()
-  if (!snapshot.empty) {
+  if (alreadyExists) {
     throw new Error('Question category already exists')
   }
 
-  const doc = await collection.add(body)
-
-  const ref = await doc.get()
-
-  return ref.data()
-}
-
-export const deleteQuestionCategory = async (body: QuestionCategoryType) => {
-  const collection = getCollection()
-
-  let query = collection as FirebaseFirestore.Query<FirebaseFirestore.DocumentData, FirebaseFirestore.DocumentData>
-  query = query.where('questionId', '==', body.questionId)
-  query = query.where('categoryId', '==', body.categoryId)
-
-  const querySnapshot = await query.get()
-  querySnapshot.forEach(async (doc) => {
-    await doc.ref.delete()
+  return await prisma.questionCategory.create({
+    data: {
+      questionId: body.questionId,
+      categoryId: body.categoryId
+    }
   })
 }
 
-export const deleteAllQuestionCategories = async () => {
-  const collection = getCollection()
-
-  const snapshot = await collection.get()
-  snapshot.forEach(async (doc) => {
-    await doc.ref.delete()
+export const deleteQuestionCategory = async (body: {
+  questionId: string
+  categoryId: string
+}): Promise<void> => {
+  await prisma.questionCategory.deleteMany({
+    where: {
+      questionId: body.questionId,
+      categoryId: body.categoryId
+    }
   })
 }
 
-export const deleteQuestionCategoriesByCategoryId = async (categoryId: string): Promise<void> => {
-  const collection = getCollection()
-
-  const query = collection.where('categoryId', '==', categoryId)
-  const snapshot = await query.get()
-  snapshot.forEach(async (doc) => {
-    await doc.ref.delete()
+export const deleteQuestionCategoriesByCategoryId = async (
+  categoryId: string
+): Promise<void> => {
+  await prisma.questionCategory.deleteMany({
+    where: {
+      categoryId
+    }
   })
 }
 
-export const getQuestionsOfCategory = async (categoryId: string) => {
-  const collection = getCollection()
-
-  const query = collection.where('categoryId', '==', categoryId)
-  const snapshot = await query.get()
-
-  const questionCategories = snapshot.docs.map((doc) => doc.data() as QuestionCategoryType)
-
-  return questionCategories
+export const getQuestionsOfCategory = async (categoryId: string): Promise<QuestionCategory[]> => {
+  return prisma.questionCategory.findMany({
+    where: { categoryId }
+  })
 }
 
-export const getAllQuestionCategories = async (): Promise<QuestionCategoryType[]> => {
-  const collection = getCollection()
-
-  const snapshot = await collection.get()
-
-  const questionCategories = snapshot.docs.map((doc) => doc.data() as QuestionCategoryType)
-
-  return questionCategories
+export const getAllQuestionCategories = async (): Promise<QuestionCategory[]> => {
+  return prisma.questionCategory.findMany({})
 }
--- models/userAnswerTotals.ts
+++ models/userAnswerTotals.ts
@@ -1,49 +1,19 @@
-import { createDb } from '../pages/api/utils/firestore'
-
-export type UserAnswerTotalType = {
-  id: string
-  fid: number
-  count: number
-  lastUpdated: number
-}
-
-let collection: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>
-
-const getCollection = () => {
-  if (collection) {
-    return collection
-  }
-
-  const db = createDb()
-  collection = db.collection('userAnswerTotal')
-  return collection
-}
+import { UserAnswerTotal } from "@prisma/client"
+import { prisma } from "@/prisma"
 
 export const getPageOfTopUserAnswerTotals = async (params: {
   limit: number
-  lastId?: string
-}): Promise<UserAnswerTotalType[]> => {
-  const col = getCollection()
-  let query = col.orderBy("lastUpdated", "desc").limit(params.limit)
-
-  if (params.lastId) {
-    const lastDoc = await col.doc(params.lastId).get()
-    if (lastDoc) {
-      query = query.startAfter(lastDoc)
+  skip: number
+}): Promise<UserAnswerTotal[]> => {
+  return prisma.userAnswerTotal.findMany({
+    take: params.limit,
+    skip: params.skip,
+    orderBy: {
+      lastUpdated: "desc"
     }
-  }
-
-  const querySnapshot = await query.get()
-
-  return querySnapshot.docs.map((doc) => {
-    const data = doc.data() as UserAnswerTotalType
-    data.id = doc.id
-    return data
   })
 }
 
 export const countUserAnswerTotals = async (): Promise<number> => {
-  const col = getCollection()
-  const snapshot = await col.get()
-  return snapshot.size
+  return prisma.userAnswerTotal.count()
 }
